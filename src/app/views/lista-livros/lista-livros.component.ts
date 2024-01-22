b903b8871b459d8d1a47622f212459bd083271bc
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-01-22 17:03:44 -0300

    busca e exibição dos livros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Item } from './../../models/interfaces';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
+  campoBusca: string = ''
+  subscription: Subscription
+  livro: Livro
 
-  constructor() { }
+  constructor(private service: LivroService) { }
 
+  buscarLivros(){
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.livrosResultadosParaLivros(items)
+      },
+      error: erro => console.log(erro),
+      // complete: () => console.log('Observable completado')
+    }
+    )
+  }
+
+  livrosResultadosParaLivros(items: Item[]): LivroVolumeInfo[]{
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
+  }
 }
 
 
